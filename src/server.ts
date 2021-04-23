7809fe65442ddc95b71544363e16316c27b01829
makaifox <yu.trindade@gmail.com>
2021-04-23 16:55:25 -0300

    inicio aula4

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,29 @@
 import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import path from "path";
 
 import "./database";
 
 import { routes } from "./routes"
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.set("views", path.join(__dirname, "..", "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get("/pages/client", (request, response) => {
+  return response.render("html/client.html")
+})
+
+const http = createServer(app);// Criando protocolo http
+const io = new Server(http);// Criando protocolo websockets
+
+io.on("connection", (socket: Socket) => {
+  console.log("Se conectou", socket.id);
+});
 
 app.use(express.json());
 
@@ -18,4 +37,4 @@ app.use(routes);
  * PATCH = Alterar uma informação especifica
  */
 
-app.listen(3333, () => console.log("Server is running on port 3333"));
+http.listen(3333, () => console.log("Server is running on port 3333"));
